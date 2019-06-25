40d67e98ea3949aa39ea98aeba794afb452e1107
Daniel-Stafford <stafford.daniel@gmail.com>
2019-06-25 12:34:21 +0300

    added references

--- src/pages/References.js
+++ src/pages/References.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Breadcrumb, Container } from 'react-bootstrap';
+import { Breadcrumb, Container, Card } from 'react-bootstrap';
 import '../style/style.css';
 
 const References = () => {
@@ -7,10 +7,24 @@ const References = () => {
     <>
       <Container fluid='true' className='container'>
         <h2>References</h2>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta
-          sapiente pariatur, suscipit voluptatum odio tempora!
-        </p>
+        <Card>
+          <Card.Header />
+          <Card.Body>
+            <blockquote className='blockquote mb-0'>
+              <p>He's pretty tolerable, I guess.</p>
+              <footer className='blockquote-footer'>My girlfriend</footer>
+            </blockquote>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Header />
+          <Card.Body>
+            <blockquote className='blockquote mb-0'>
+              <p>He makes good coffee.</p>
+              <footer className='blockquote-footer'>Former roommate</footer>
+            </blockquote>
+          </Card.Body>
+        </Card>
       </Container>
       <Breadcrumb>
         <Breadcrumb.Item href='/'>Home</Breadcrumb.Item>
