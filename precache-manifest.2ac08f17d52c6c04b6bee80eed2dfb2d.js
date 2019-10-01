8ecedfbd6691986cff667906e46043b93b0792ed
Daniel-Stafford <stafford.daniel@gmail.com>
2019-10-01 12:01:29 +0300

    Updates

---
+++ precache-manifest.2ac08f17d52c6c04b6bee80eed2dfb2d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7638e40d88446487cab8b09e1e8629a1",
+    "url": "%20//daniel-stafford.github.io/react-boostrap-portfolio-site/index.html"
+  },
+  {
+    "revision": "d4801abc7c4bd5c0b9bd",
+    "url": "%20//daniel-stafford.github.io/react-boostrap-portfolio-site/static/css/main.3a4fd712.chunk.css"
+  },
+  {
+    "revision": "26108401aee286f65642",
+    "url": "%20//daniel-stafford.github.io/react-boostrap-portfolio-site/static/js/2.114ee16b.chunk.js"
+  },
+  {
+    "revision": "d4801abc7c4bd5c0b9bd",
+    "url": "%20//daniel-stafford.github.io/react-boostrap-portfolio-site/static/js/main.85107801.chunk.js"
+  },
+  {
+    "revision": "4f8af5ebe7b4c1f05e29",
+    "url": "%20//daniel-stafford.github.io/react-boostrap-portfolio-site/static/js/runtime~main.33020eaa.js"
+  },
+  {
+    "revision": "e70b9bf59809246963638a9bf065d480",
+    "url": "%20//daniel-stafford.github.io/react-boostrap-portfolio-site/static/media/Profile.e70b9bf5.pdf"
+  },
+  {
+    "revision": "1c1e071fb6f7975f13a864dd790e6107",
+    "url": "%20//daniel-stafford.github.io/react-boostrap-portfolio-site/static/media/Resume.1c1e071f.docx"
+  },
+  {
+    "revision": "ceee52e70cb2627860efd8186dce8e62",
+    "url": "%20//daniel-stafford.github.io/react-boostrap-portfolio-site/static/media/adoption.ceee52e7.png"
+  },
+  {
+    "revision": "0ac0ba7d66c675328b0ba1cc0cd066a9",
+    "url": "%20//daniel-stafford.github.io/react-boostrap-portfolio-site/static/media/lunch.0ac0ba7d.png"
+  },
+  {
+    "revision": "3c84d9acbfc8bbd258447860b99b58f9",
+    "url": "%20//daniel-stafford.github.io/react-boostrap-portfolio-site/static/media/numbers.3c84d9ac.png"
+  },
+  {
+    "revision": "94110fff1b76acc7707a755983096366",
+    "url": "%20//daniel-stafford.github.io/react-boostrap-portfolio-site/static/media/portfolio.94110fff.png"
+  },
+  {
+    "revision": "e8fe331770d5a8fc1d79fcbfc007a791",
+    "url": "%20//daniel-stafford.github.io/react-boostrap-portfolio-site/static/media/profile2.e8fe3317.jpg"
+  },
+  {
+    "revision": "cf2ff452c1bbb89c40b95ba11751b02e",
+    "url": "%20//daniel-stafford.github.io/react-boostrap-portfolio-site/static/media/profile3.cf2ff452.jpeg"
+  }
+]);
